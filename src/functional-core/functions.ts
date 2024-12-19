ed9b371ad54727627606931c459f9d4d918c1c35
Kylian Bardini <kylianb1@icloud.com>
2024-12-19 15:42:18 +0100

    🚩 feat: create updateScore fn

--- src/functional-core/functions.ts
+++ src/functional-core/functions.ts
@@ -3,3 +3,11 @@ import type { History, Score } from "./types";
 export const initialScore: Score = { teamA: 0, teamB: 0 };
 export const randomShot = (): boolean => Math.random() < 0.5;
 
+export const updateScore = (
+  score: Score,
+  team: "A" | "B",
+  result: boolean,
+): Score => {
+  if (team === "A") return { ...score, teamA: score.teamA + (result ? 1 : 0) };
+  else return { ...score, teamB: score.teamB + (result ? 1 : 0) };
+};

2339ad1e7b89d7c98e2c964f459420c861bbc8e3
Kylian Bardini <kylianb1@icloud.com>
2024-12-19 15:56:55 +0100

    ✨ feat: create imperative-shell

--- src/imperative-shell/index.ts
+++ src/imperative-shell/index.ts
@@ -1,12 +1,32 @@
-import { increment } from "../functional-core/functions";
+import {
+  addHistory,
+  checkWinner,
+  displayHistory,
+  initialScore,
+  randomShot,
+  updateScore,
+} from "../functional-core/functions";
+import type { History, Score } from "../functional-core/types";
 
-const counterObj = Object.freeze({ kikoo: { value: 1 } });
-counterObj.kikoo.value = 2;
+const simulatePenaltyShootout = (): void => {
+  let score: Score = initialScore;
+  let history: History = [];
+  let shots = 0;
+  let winner: string | null = null;
 
-const counter: number = 1;
-const counter2: number = increment(counter);
+  while (!winner) {
+    const team = shots % 2 === 0 ? "A" : "B";
+    const result = randomShot();
 
-console.assert(counter === 1);
-console.assert(increment(counter) === 2);
+    score = updateScore(score, team, result);
+    history = addHistory(history, shots + 1, score, result, team);
+    winner = checkWinner(score, shots + 1);
 
-console.log(counter2);
+    shots++;
+  }
+
+  displayHistory(history);
+  console.log(`Victoire : ${winner} (Score : ${score.teamA}/${score.teamB})`);
+};
+
+simulatePenaltyShootout();

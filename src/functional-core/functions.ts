ed085abf5707f8a5ef513514582c4fe6e8fa2729
Kylian Bardini <kylianb1@icloud.com>
2024-12-19 15:43:16 +0100

    🚩 feat: create addHistory fn

--- src/functional-core/functions.ts
+++ src/functional-core/functions.ts
@@ -11,3 +11,17 @@ export const updateScore = (
   if (team === "A") return { ...score, teamA: score.teamA + (result ? 1 : 0) };
   else return { ...score, teamB: score.teamB + (result ? 1 : 0) };
 };
+
+export const addHistory = (
+  history: History,
+  shot: number,
+  score: Score,
+  result: boolean,
+  team: "A" | "B",
+): History => {
+  const resultStr =
+    team === "A"
+      ? `Équipe A : ${result ? "+1" : "0"}`
+      : `Équipe B : ${result ? "+1" : "0"}`;
+  return [...history, { shot, score, result: resultStr }];
+};
